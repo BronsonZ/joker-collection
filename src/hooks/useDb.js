0d8eedd0b7cb4bddd01ea87a4575127935a4fde7
BronsonZ <bzell007@gmail.com>
2022-02-18 16:52:41 -0500

    fixing async useEffect

--- src/hooks/useDb.js
+++ src/hooks/useDb.js
@@ -1,21 +1,25 @@
 import { useEffect, useState } from "react";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, getDocs } from "firebase/firestore";
 import { db } from "../firebase/config";
 
 const useDb = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(async () => {
-    const q = query(collection(db, "jokers"));
+    const getData = async () => {
+      const q = query(collection(db, "jokers"));
 
-    const querySnapshot = await getDocs(q);
+      const querySnapshot = await getDocs(q);
 
-    let docs = [];
+      let docs = [];
 
-    querySnapshot.forEach((doc) => {
-      docs.push({ ...doc.data(), id: doc.id });
-    });
-    setPosts(docs);
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+      setPosts(docs);
+    };
+
+    getData();
   }, []);
 
   return { posts };
