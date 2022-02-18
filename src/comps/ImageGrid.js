59cac45aa949ed76aa398d3e491c0aa922dfad65
BronsonZ <bzell007@gmail.com>
2022-02-18 17:00:21 -0500

    minor fixes to ImageGrid

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -6,14 +6,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const ImageGrid = () => {
   const { posts } = useDb();
   return (
-    <Container className="mt-3" fluid="sm">
+    <Container  className="mt-3" fluid="sm">
       <Row xs={2} sm={2} md={3} lg={4}>
         {posts &&
           posts.map((post) => (
-            <Col key={post.id}>
+            <Col style={{whiteSpace: 'pre-wrap', overflowWrap: 'break-word'}} key={post.id}>
               <h3>{post.name}</h3>
               <Image width="80%" rounded fluid src={post.imageUrl} />
-              <p>Description:{post.desc} Price: {post.price}</p>
+              <p className="mb-0">Description: {post.desc}</p>
+              <p>Price: {post.price}</p>
             </Col>
           ))}
       </Row>
