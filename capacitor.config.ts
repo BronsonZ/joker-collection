bea082f94cee66c75de2932ac2eaed07bb2aed37
BronsonZ <bzell007@gmail.com>
2022-03-07 21:47:02 -0500

    initial capacitor set up

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jokerbabies.app',
+  appName: 'joker-collection',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
