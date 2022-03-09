2028873946f1cfcf6db07454690ceccae69473bd
BronsonZ <bzell007@gmail.com>
2022-03-09 15:05:43 -0500

    remove service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,4 +12,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-serviceWorker.register();
