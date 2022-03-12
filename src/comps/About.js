484cb921e61a611cbb35c0bf56f55b311237b9c2
BronsonZ <bzell007@gmail.com>
2022-03-12 17:50:12 -0500

    about page, change hover opacity

--- src/comps/About.js
+++ src/comps/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <Container className="mt-3 text-center" style={{fontSize: "1.2em"}}>
             <h1>About</h1>
             <p>This is a website for Mindi to keep track of and display her entire Joker collection.</p>
-            <a className="text-reset" href="https://github.com/BronsonZ/joker-collection">Source Code</a>
+            <a className="stats-link text-reset" href="https://github.com/BronsonZ/joker-collection">Source Code</a>
         </Container>
     )
 }
