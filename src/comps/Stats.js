90ef2fbb6fb85283c159910f3939edd51c52dc3a
BronsonZ <bzell007@gmail.com>
2022-08-06 03:34:48 -0400

    update and improve stats page

--- src/comps/Stats.js
+++ src/comps/Stats.js
@@ -1,21 +1,15 @@
 import useDb from "../hooks/useDb";
-import useFilter from "../hooks/useFilter";
 import { Container, Table } from "react-bootstrap"
 import { Link } from "react-router-dom";
 
 const Stats = () => {
     const { posts: allJokers, loading: loadingAll } = useDb(false, "jokers");
-    const { filtered: popJokers} = useFilter(allJokers, "pop");
-    const { filtered: fJokers} = useFilter(allJokers, "figurine");
-    const { filtered: keychainJokers} = useFilter(allJokers, "keychain");
-    const { filtered: afJokers} = useFilter(allJokers, "actionFigure");
-    const { filtered: otherJokers} = useFilter(allJokers, "other");
-    const numberOfJokers = allJokers.length;
-    const numberOfAf = afJokers.length;
-    const numberOfPops = popJokers.length;
-    const numberOfFigurines = fJokers.length;
-    const numberOfKeychains = keychainJokers.length;
-    const numberOfOther = otherJokers.length;
+    let numberOfJokers = allJokers.length;
+    let numberOfAf = 0;
+    let numberOfPops = 0;
+    let numberOfFigurines = 0;
+    let numberOfKeychains = 0;
+    let numberOfOther = 0;
     let totalCost = 0;
     let afCost = 0;
     let popCost = 0;
@@ -25,22 +19,39 @@ const Stats = () => {
 
     for(let i = 0; i<numberOfJokers; ++i){
         totalCost+=allJokers[i].price;
+        switch(allJokers[i].category){
+            case "actionFigure":
+                afCost+=allJokers[i].price;
+                numberOfAf++;
+                break;
+            case "pop":
+                popCost+=allJokers[i].price;
+                numberOfPops++;
+                break;
+            case "figurine":
+                fCost+=allJokers[i].price;
+                numberOfFigurines++;
+                break;
+            case "keychain":
+                keychainCost+=allJokers[i].price;
+                numberOfKeychains++;
+                break;
+            case "other":
+                otherCost+=allJokers[i].price;
+                numberOfOther++;
+                break;
+            default:
+                break;
+        }
     }
-    for(let i = 0; i<numberOfPops; ++i){
-        popCost+=popJokers[i].price;
-    }
-    for(let i = 0; i<numberOfAf; ++i){
-        afCost+=afJokers[i].price;
-    }
-    for(let i = 0; i<numberOfFigurines; ++i){
-        fCost+=fJokers[i].price;
-    }
-    for(let i = 0; i<numberOfKeychains; ++i){
-        keychainCost+=keychainJokers[i].price;
-    }
-    for(let i = 0; i<numberOfOther; ++i){
-        otherCost+=otherJokers[i].price;
-    }
+
+    let popAverage = numberOfPops > 0 ? Math.round(popCost/numberOfPops) : 0;
+    let afAverage = numberOfAf > 0 ? Math.round(afCost/numberOfAf) : 0;
+    let fAverage = numberOfFigurines > 0 ? Math.round(fCost/numberOfFigurines) : 0;
+    let keychainAverage = numberOfKeychains > 0 ? Math.round(keychainCost/numberOfKeychains) : 0;
+    let otherAverage = numberOfOther > 0 ? Math.round(otherCost/numberOfOther) : 0;
+    let totalAverage = numberOfJokers > 0 ? Math.round(totalCost/numberOfJokers) : 0;
+    
     return (
         <Container className="mt-3 text-center text-wrap">
             <h1>Stats of all the Jokers</h1>
@@ -58,31 +69,31 @@ const Stats = () => {
                         <td><Link className="stats-link text-reset" to="/?=pop">Pops</Link></td>
                         <td>{!loadingAll && numberOfPops}</td>
                         <td>${popCost}</td>
-                        <td>${!loadingAll && popCost>0 ? Math.round(popCost/numberOfPops) : 0}</td>
+                        <td>${!loadingAll && popAverage}</td>
                     </tr>
                     <tr>
                         <td><Link className="stats-link text-reset" to="/?=figurine">Figurines</Link></td>
                         <td>{!loadingAll && numberOfFigurines}</td>
                         <td>${fCost}</td>
-                        <td>${!loadingAll && fCost>0 ? Math.round(fCost/numberOfFigurines) : 0}</td>
+                        <td>${!loadingAll && fAverage}</td>
                     </tr>
                     <tr>
                         <td><Link className="stats-link text-reset" to="/?=actionFigure">Action Figures</Link></td>
                         <td>{!loadingAll && numberOfAf}</td>
                         <td>${afCost}</td>
-                        <td>${!loadingAll && afCost>0 ? Math.round(afCost/numberOfAf) : 0}</td>
+                        <td>${!loadingAll && afAverage}</td>
                     </tr>
                     <tr>
                         <td><Link className="stats-link text-reset" to="/?=keychain">Keychains</Link></td>
                         <td>{!loadingAll && numberOfKeychains}</td>
                         <td>${keychainCost}</td>
-                        <td>${!loadingAll && keychainCost>0 ? Math.round(keychainCost/numberOfKeychains) : 0}</td>
+                        <td>${!loadingAll && keychainAverage}</td>
                     </tr>
                     <tr>
                         <td><Link className="stats-link text-reset" to="/?=other">Others</Link></td>
                         <td>{!loadingAll && numberOfOther}</td>
                         <td>${otherCost}</td>
-                        <td>${!loadingAll && otherCost>0 ? Math.round(otherCost/numberOfOther) : 0}</td>
+                        <td>${!loadingAll && otherAverage}</td>
                     </tr>
                     
                 </tbody>
@@ -91,7 +102,7 @@ const Stats = () => {
                         <td><Link className="stats-link text-reset" to="/?=">Total</Link></td>
                         <td>{!loadingAll && numberOfJokers}</td>
                         <td>${totalCost}</td>
-                        <td>${!loadingAll && totalCost>0 ? Math.round(totalCost/numberOfJokers) : 0}</td>
+                        <td>${!loadingAll && totalAverage}</td>
                     </tr>
                 </tfoot>
             </Table>
