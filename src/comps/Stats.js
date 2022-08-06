f6b19e78234efaeac9a78c07d136851b5c4e86c7
BronsonZ <bzell007@gmail.com>
2022-08-06 05:08:30 -0400

    fix stats table size

--- src/comps/Stats.js
+++ src/comps/Stats.js
@@ -67,7 +67,7 @@ const Stats = () => {
     return (
         <Container className="mt-3 text-center text-wrap">
             <h1>Stats of all the Jokers</h1>
-            {!loading && <Table className="text-success" variant="dark">
+            {!loading && <Table size="sm" className="text-success" variant="dark">
                 <thead>
                     <tr>
                         <th></th>
